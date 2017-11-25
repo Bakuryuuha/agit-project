1d76acdd4b325c350f75256e5f1924d8a7e2a47d
kevin <kevindreyar@yahoo.com>
2017-11-25 17:10:19 +0700

    init
    init

---
+++ storage/framework/classes.php
@@ -0,0 +1,12 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins\\october\\demo\\Plugin.php',
+  'backend\\Controllers\\index' => 'modules\\backend\\controllers\\index.php',
+  'backend\\Controllers\\auth' => 'modules\\backend\\controllers\\auth.php',
+  'system\\Controllers\\updates' => 'modules\\system\\controllers\\updates.php',
+  'cms\\Controllers\\themes' => 'modules\\cms\\controllers\\themes.php',
+  'system\\Controllers\\settings' => 'modules\\system\\controllers\\settings.php',
+  'backend\\Controllers\\accesslogs' => 'modules\\backend\\controllers\\accesslogs.php',
+  'backend\\Controllers\\users' => 'modules\\backend\\controllers\\users.php',
+  'backend\\Controllers\\userroles' => 'modules\\backend\\controllers\\userroles.php',
+  'system\\Controllers\\eventlogs' => 'modules\\system\\controllers\\eventlogs.php',
+);
